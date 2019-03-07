Abdulmoiz-Ahmer/android

Moved auto upload preferences to AppPreferences
Signed-off-by: Chris Narkiewicz <hello@ezaquarii.com>

src/main/java/com/nextcloud/client/preferences/AppPreferences.java
@@ -86,6 +86,37 @@ public interface AppPreferences {
      */
     void setLastUploadPath(String path);
 
+    /**
+     * Gets the auto upload paths flag last set.
+     *
+     * @return ascending order     the legacy cleaning flag, default is false
+     */
+    boolean getAutoUploadPathsUpdate();
+
+    /**
+     * Saves the legacy cleaning flag which the user has set last.
+     *
+     * @param pathUpdate flag if it is a auto upload path update
+     */
+    void setAutoUploadPathsUpdate(boolean pathUpdate);
+
+    /**
+     * Gets the auto upload split out flag last set.
+     *
+     * @return ascending order     the legacy cleaning flag, default is false
+     */
+    boolean getAutoUploadSplitEntries();
+
+    /**
+     * Saves the flag for split entries magic
+     *
+     * @param splitOut flag if it is a auto upload path update
+     */
+    void setAutoUploadSplitEntries(boolean splitOut);
+
+    boolean getAutoUploadInit();
+    void setAutoUploadInit(boolean autoUploadInit);
+
     boolean isShowDetailedTimestampEnabled();
     void setShowDetailedTimestampEnabled(boolean showDetailedTimestamp);
 

src/main/java/com/nextcloud/client/preferences/PreferenceManager.java
@@ -354,10 +354,6 @@ public final class PreferenceManager implements AppPreferences {
         return preferenceName + "_" + folderIdString;
     }
 
-    public static boolean getAutoUploadInit(Context context) {
-        return getDefaultSharedPreferences(context).getBoolean(PREF__AUTO_UPLOAD_INIT, false);
-    }
-
     /**
      * Gets the legacy cleaning flag last set.
      *
@@ -376,25 +372,34 @@ public final class PreferenceManager implements AppPreferences {
         return getDefaultSharedPreferences(context).getBoolean(PREF__FIX_STORAGE_PATH, false);
     }
 
-
-    /**
-     * Gets the auto upload paths flag last set.
-     *
-     * @param context Caller {@link Context}, used to access to shared preferences manager.
-     * @return ascending order     the legacy cleaning flag, default is false
-     */
-    public static boolean getAutoUploadPathsUpdate(Context context) {
-        return getDefaultSharedPreferences(context).getBoolean(PREF__AUTO_UPLOAD_UPDATE_PATH, false);
+    @Override
+    public boolean getAutoUploadPathsUpdate() {
+        return preferences.getBoolean(PREF__AUTO_UPLOAD_UPDATE_PATH, false);
     }
 
-    /**
-     * Gets the auto upload split out flag last set.
-     *
-     * @param context Caller {@link Context}, used to access to shared preferences manager.
-     * @return ascending order     the legacy cleaning flag, default is false
-     */
-    public static boolean getAutoUploadSplitEntries(Context context) {
-        return getDefaultSharedPreferences(context).getBoolean(PREF__AUTO_UPLOAD_SPLIT_OUT, false);
+    @Override
+    public void setAutoUploadPathsUpdate(boolean pathUpdate) {
+        preferences.edit().putBoolean(PREF__AUTO_UPLOAD_UPDATE_PATH, pathUpdate).apply();
+    }
+
+    @Override
+    public boolean getAutoUploadSplitEntries() {
+        return preferences.getBoolean(PREF__AUTO_UPLOAD_SPLIT_OUT, false);
+    }
+
+    @Override
+    public void setAutoUploadSplitEntries(boolean splitOut) {
+        preferences.edit().putBoolean(PREF__AUTO_UPLOAD_SPLIT_OUT, splitOut).apply();
+    }
+
+    @Override
+    public boolean getAutoUploadInit() {
+        return preferences.getBoolean(PREF__AUTO_UPLOAD_INIT, false);
+    }
+
+    @Override
+    public void setAutoUploadInit(boolean autoUploadInit) {
+        preferences.edit().putBoolean(PREF__AUTO_UPLOAD_INIT, autoUploadInit).apply();
     }
 
     /**
@@ -413,30 +418,6 @@ public final class PreferenceManager implements AppPreferences {
 
     public static void setStoragePathFix(Context context, boolean storagePathFix) {
         saveBooleanPreference(context, PREF__FIX_STORAGE_PATH, storagePathFix);
-    }
-
-    public static void setAutoUploadInit(Context context, boolean autoUploadInit) {
-        saveBooleanPreference(context, PREF__AUTO_UPLOAD_INIT, autoUploadInit);
-    }
-
-    /**
-     * Saves the legacy cleaning flag which the user has set last.
-     *
-     * @param context    Caller {@link Context}, used to access to shared preferences manager.
-     * @param pathUpdate flag if it is a auto upload path update
-     */
-    public static void setAutoUploadPathsUpdate(Context context, boolean pathUpdate) {
-        saveBooleanPreference(context, PREF__AUTO_UPLOAD_UPDATE_PATH, pathUpdate);
-    }
-
-    /**
-     * Saves the flag for split entries magic
-     *
-     * @param context    Caller {@link Context}, used to access to shared preferences manager.
-     * @param splitOut flag if it is a auto upload path update
-     */
-    public static void setAutoUploadSplitEntries(Context context, boolean splitOut) {
-        saveBooleanPreference(context, PREF__AUTO_UPLOAD_SPLIT_OUT, splitOut);
     }
 
     /**

src/main/java/com/owncloud/android/MainApp.java
@@ -303,7 +303,8 @@ public class MainApp extends MultiDexApplication {
                     Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
                 splitOutAutoUploadEntries();
             } else {
-                PreferenceManager.setAutoUploadSplitEntries(getAppContext(), true);
+                AppPreferences preferences = PreferenceManager.fromContext(getAppContext());
+                preferences.setAutoUploadSplitEntries(true);
             }
         }
 
@@ -536,7 +537,8 @@ public class MainApp extends MultiDexApplication {
     private static void updateAutoUploadEntries() {
         // updates entries to reflect their true paths
         Context context = getAppContext();
-        if (!PreferenceManager.getAutoUploadPathsUpdate(context)) {
+        AppPreferences preferences = PreferenceManager.fromContext(context);
+        if (!preferences.getAutoUploadPathsUpdate()) {
             SyncedFolderProvider syncedFolderProvider =
                     new SyncedFolderProvider(MainApp.getAppContext().getContentResolver());
             syncedFolderProvider.updateAutoUploadPaths(mContext);
@@ -545,7 +547,8 @@ public class MainApp extends MultiDexApplication {
 
     private static void splitOutAutoUploadEntries() {
         Context context = getAppContext();
-        if (!PreferenceManager.getAutoUploadSplitEntries(context)) {
+        AppPreferences preferences = PreferenceManager.fromContext(context);
+        if (!preferences.getAutoUploadSplitEntries()) {
             // magic to split out existing synced folders in two when needed
             // otherwise, we migrate them to their proper type (image or video)
             Log_OC.i(TAG, "Migrate synced_folders records for image/video split");
@@ -593,13 +596,14 @@ public class MainApp extends MultiDexApplication {
                 syncedFolderProvider.deleteSyncedFolder(id);
             }
 
-            PreferenceManager.setAutoUploadSplitEntries(context, true);
+            preferences.setAutoUploadSplitEntries(true);
         }
     }
 
     private static void initiateExistingAutoUploadEntries() {
         new Thread(() -> {
-            if (!PreferenceManager.getAutoUploadInit(getAppContext())) {
+            AppPreferences preferences = PreferenceManager.fromContext(getAppContext());
+            if (!preferences.getAutoUploadInit()) {
                 SyncedFolderProvider syncedFolderProvider =
                         new SyncedFolderProvider(MainApp.getAppContext().getContentResolver());
 
@@ -609,7 +613,7 @@ public class MainApp extends MultiDexApplication {
                     }
                 }
 
-                PreferenceManager.setAutoUploadInit(getAppContext(), true);
+                preferences.setAutoUploadInit(true);
             }
 
         }).start();

src/main/java/com/owncloud/android/datamodel/SyncedFolderProvider.java
@@ -27,6 +27,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 
+import com.nextcloud.client.preferences.AppPreferences;
 import com.nextcloud.client.preferences.PreferenceManager;
 import com.owncloud.android.db.ProviderMeta;
 import com.owncloud.android.lib.common.utils.Log_OC;
@@ -262,7 +263,8 @@ public class SyncedFolderProvider extends Observable {
         }
 
         if (context != null) {
-            PreferenceManager.setAutoUploadPathsUpdate(context, true);
+            AppPreferences preferences = PreferenceManager.fromContext(context);
+            preferences.setAutoUploadPathsUpdate(true);
         }
     }
 

PhiGi87/souvera_android

src/main/java/com/nextcloud/client/preferences/AppPreferences.java
@@ -86,6 +86,37 @@ public interface AppPreferences {
      */
     void setLastUploadPath(String path);
 
+    /**
+     * Gets the auto upload paths flag last set.
+     *
+     * @return ascending order     the legacy cleaning flag, default is false
+     */
+    boolean isAutoUploadPathsUpdateEnabled();
+
+    /**
+     * Saves the legacy cleaning flag which the user has set last.
+     *
+     * @param pathUpdate flag if it is a auto upload path update
+     */
+    void setAutoUploadPathsUpdateEnabled(boolean pathUpdate);
+
+    /**
+     * Gets the auto upload split out flag last set.
+     *
+     * @return ascending order     the legacy cleaning flag, default is false
+     */
+    boolean isAutoUploadSplitEntriesEnabled();
+
+    /**
+     * Saves the flag for split entries magic
+     *
+     * @param splitOut flag if it is a auto upload path update
+     */
+    void setAutoUploadSplitEntriesEnabled(boolean splitOut);
+
+    boolean isAutoUploadInitialized();
+    void setAutoUploadInit(boolean autoUploadInit);
+
     boolean isShowDetailedTimestampEnabled();
     void setShowDetailedTimestampEnabled(boolean showDetailedTimestamp);
 

src/main/java/com/nextcloud/client/preferences/PreferenceManager.java
@@ -354,10 +354,6 @@ public final class PreferenceManager implements AppPreferences {
         return preferenceName + "_" + folderIdString;
     }
 
-    public static boolean getAutoUploadInit(Context context) {
-        return getDefaultSharedPreferences(context).getBoolean(PREF__AUTO_UPLOAD_INIT, false);
-    }
-
     /**
      * Gets the legacy cleaning flag last set.
      *
@@ -376,25 +372,34 @@ public final class PreferenceManager implements AppPreferences {
         return getDefaultSharedPreferences(context).getBoolean(PREF__FIX_STORAGE_PATH, false);
     }
 
-
-    /**
-     * Gets the auto upload paths flag last set.
-     *
-     * @param context Caller {@link Context}, used to access to shared preferences manager.
-     * @return ascending order     the legacy cleaning flag, default is false
-     */
-    public static boolean getAutoUploadPathsUpdate(Context context) {
-        return getDefaultSharedPreferences(context).getBoolean(PREF__AUTO_UPLOAD_UPDATE_PATH, false);
+    @Override
+    public boolean isAutoUploadPathsUpdateEnabled() {
+        return preferences.getBoolean(PREF__AUTO_UPLOAD_UPDATE_PATH, false);
     }
 
-    /**
-     * Gets the auto upload split out flag last set.
-     *
-     * @param context Caller {@link Context}, used to access to shared preferences manager.
-     * @return ascending order     the legacy cleaning flag, default is false
-     */
-    public static boolean getAutoUploadSplitEntries(Context context) {
-        return getDefaultSharedPreferences(context).getBoolean(PREF__AUTO_UPLOAD_SPLIT_OUT, false);
+    @Override
+    public void setAutoUploadPathsUpdateEnabled(boolean pathUpdate) {
+        preferences.edit().putBoolean(PREF__AUTO_UPLOAD_UPDATE_PATH, pathUpdate).apply();
+    }
+
+    @Override
+    public boolean isAutoUploadSplitEntriesEnabled() {
+        return preferences.getBoolean(PREF__AUTO_UPLOAD_SPLIT_OUT, false);
+    }
+
+    @Override
+    public void setAutoUploadSplitEntriesEnabled(boolean splitOut) {
+        preferences.edit().putBoolean(PREF__AUTO_UPLOAD_SPLIT_OUT, splitOut).apply();
+    }
+
+    @Override
+    public boolean isAutoUploadInitialized() {
+        return preferences.getBoolean(PREF__AUTO_UPLOAD_INIT, false);
+    }
+
+    @Override
+    public void setAutoUploadInit(boolean autoUploadInit) {
+        preferences.edit().putBoolean(PREF__AUTO_UPLOAD_INIT, autoUploadInit).apply();
     }
 
     /**
@@ -413,30 +418,6 @@ public final class PreferenceManager implements AppPreferences {
 
     public static void setStoragePathFix(Context context, boolean storagePathFix) {
         saveBooleanPreference(context, PREF__FIX_STORAGE_PATH, storagePathFix);
-    }
-
-    public static void setAutoUploadInit(Context context, boolean autoUploadInit) {
-        saveBooleanPreference(context, PREF__AUTO_UPLOAD_INIT, autoUploadInit);
-    }
-
-    /**
-     * Saves the legacy cleaning flag which the user has set last.
-     *
-     * @param context    Caller {@link Context}, used to access to shared preferences manager.
-     * @param pathUpdate flag if it is a auto upload path update
-     */
-    public static void setAutoUploadPathsUpdate(Context context, boolean pathUpdate) {
-        saveBooleanPreference(context, PREF__AUTO_UPLOAD_UPDATE_PATH, pathUpdate);
-    }
-
-    /**
-     * Saves the flag for split entries magic
-     *
-     * @param context    Caller {@link Context}, used to access to shared preferences manager.
-     * @param splitOut flag if it is a auto upload path update
-     */
-    public static void setAutoUploadSplitEntries(Context context, boolean splitOut) {
-        saveBooleanPreference(context, PREF__AUTO_UPLOAD_SPLIT_OUT, splitOut);
     }
 
     @Override

src/main/java/com/owncloud/android/MainApp.java
@@ -303,7 +303,8 @@ public class MainApp extends MultiDexApplication {
                     Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
                 splitOutAutoUploadEntries();
             } else {
-                PreferenceManager.setAutoUploadSplitEntries(getAppContext(), true);
+                AppPreferences preferences = PreferenceManager.fromContext(getAppContext());
+                preferences.setAutoUploadSplitEntriesEnabled(true);
             }
         }
 
@@ -536,7 +537,8 @@ public class MainApp extends MultiDexApplication {
     private static void updateAutoUploadEntries() {
         // updates entries to reflect their true paths
         Context context = getAppContext();
-        if (!PreferenceManager.getAutoUploadPathsUpdate(context)) {
+        AppPreferences preferences = PreferenceManager.fromContext(context);
+        if (!preferences.isAutoUploadPathsUpdateEnabled()) {
             SyncedFolderProvider syncedFolderProvider =
                     new SyncedFolderProvider(MainApp.getAppContext().getContentResolver());
             syncedFolderProvider.updateAutoUploadPaths(mContext);
@@ -545,7 +547,8 @@ public class MainApp extends MultiDexApplication {
 
     private static void splitOutAutoUploadEntries() {
         Context context = getAppContext();
-        if (!PreferenceManager.getAutoUploadSplitEntries(context)) {
+        AppPreferences preferences = PreferenceManager.fromContext(context);
+        if (!preferences.isAutoUploadSplitEntriesEnabled()) {
             // magic to split out existing synced folders in two when needed
             // otherwise, we migrate them to their proper type (image or video)
             Log_OC.i(TAG, "Migrate synced_folders records for image/video split");
@@ -593,13 +596,14 @@ public class MainApp extends MultiDexApplication {
                 syncedFolderProvider.deleteSyncedFolder(id);
             }
 
-            PreferenceManager.setAutoUploadSplitEntries(context, true);
+            preferences.setAutoUploadSplitEntriesEnabled(true);
         }
     }
 
     private static void initiateExistingAutoUploadEntries() {
         new Thread(() -> {
-            if (!PreferenceManager.getAutoUploadInit(getAppContext())) {
+            AppPreferences preferences = PreferenceManager.fromContext(getAppContext());
+            if (!preferences.isAutoUploadInitialized()) {
                 SyncedFolderProvider syncedFolderProvider =
                         new SyncedFolderProvider(MainApp.getAppContext().getContentResolver());
 
@@ -609,7 +613,7 @@ public class MainApp extends MultiDexApplication {
                     }
                 }
 
-                PreferenceManager.setAutoUploadInit(getAppContext(), true);
+                preferences.setAutoUploadInit(true);
             }
 
         }).start();

src/main/java/com/owncloud/android/datamodel/SyncedFolderProvider.java
@@ -27,6 +27,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 
+import com.nextcloud.client.preferences.AppPreferences;
 import com.nextcloud.client.preferences.PreferenceManager;
 import com.owncloud.android.db.ProviderMeta;
 import com.owncloud.android.lib.common.utils.Log_OC;
@@ -262,7 +263,8 @@ public class SyncedFolderProvider extends Observable {
         }
 
         if (context != null) {
-            PreferenceManager.setAutoUploadPathsUpdate(context, true);
+            AppPreferences preferences = PreferenceManager.fromContext(context);
+            preferences.setAutoUploadPathsUpdateEnabled(true);
         }
     }
 