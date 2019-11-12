developerscontinusys/android

Remove out-of-band setAccount/onAccountSet calls
Activity account is set in FilesActivity.onAccount(),
but related components are updated in BaseActivity.onStart().

1. Initialize account in BaseActivity.onCreate()
2. Update storage manager and capabilities immediately
   on account set, not waiting for onStart() lifecycle.
3. inline storage manager and capabilities update in setAccount()
4. Remove setAccount()/onAccountSet() calls that sets default
   account, as this duplicates BaseActivity.onCreate()

Signed-off-by: Chris Narkiewicz <hello@ezaquarii.com>

src/main/java/com/owncloud/android/ui/activity/BaseActivity.java
@@ -8,6 +8,7 @@ import android.accounts.OperationCanceledException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.PersistableBundle;
 
 import com.nextcloud.client.account.UserAccountManager;
 import com.nextcloud.client.di.Injectable;
@@ -19,6 +20,7 @@ import com.owncloud.android.lib.resources.status.OCCapability;
 
 import javax.inject.Inject;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
@@ -46,6 +48,14 @@ public abstract class BaseActivity extends AppCompatActivity implements Injectab
 
     public UserAccountManager getUserAccountManager() {
         return accountManager;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        Account account = accountManager.getCurrentAccount();
+        setAccount(account, false);
     }
 
     @Override
@@ -91,6 +101,11 @@ public abstract class BaseActivity extends AppCompatActivity implements Injectab
         } else {
             swapToDefaultAccount();
         }
+
+        if(mCurrentAccount != null) {
+            mStorageManager = new FileDataStorageManager(mCurrentAccount, getContentResolver());
+            mCapabilities = mStorageManager.getCapability(mCurrentAccount.name);
+        }
     }
 
     /**
@@ -134,7 +149,7 @@ public abstract class BaseActivity extends AppCompatActivity implements Injectab
      */
     @Deprecated
     protected void onAccountSet() {
-        if (getAccount() != null) {
+        if (mCurrentAccount != null) {
             mStorageManager = new FileDataStorageManager(getAccount(), getContentResolver());
             mCapabilities = mStorageManager.getCapability(mCurrentAccount.name);
         } else {
@@ -166,15 +181,6 @@ public abstract class BaseActivity extends AppCompatActivity implements Injectab
      */
     public Account getAccount() {
         return mCurrentAccount;
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        if(mCurrentAccount != null) {
-            onAccountSet();
-        }
     }
 
     public FileDataStorageManager getStorageManager() {

PhiGi87/souvera_android

src/main/java/com/owncloud/android/ui/activity/BaseActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.PersistableBundle;
 
 import com.nextcloud.client.account.UserAccountManager;
 import com.nextcloud.client.di.Injectable;
@@ -96,9 +97,10 @@ public abstract class BaseActivity extends AppCompatActivity implements Injectab
         }
     }
 
-    @Override
-    protected void onPostResume() {
-        super.onPostResume();
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        Account account = accountManager.getCurrentAccount();
+        setAccount(account, false);
     }
 
     @Override
@@ -152,6 +154,11 @@ public abstract class BaseActivity extends AppCompatActivity implements Injectab
         } else {
             swapToDefaultAccount();
         }
+
+        if(currentAccount != null) {
+            storageManager = new FileDataStorageManager(currentAccount, getContentResolver());
+            capabilities = storageManager.getCapability(currentAccount.name);
+        }
     }
 
     /**
@@ -189,26 +196,6 @@ public abstract class BaseActivity extends AppCompatActivity implements Injectab
     }
 
     /**
-     * Called when the ownCloud {@link Account} associated to the Activity was just updated.
-     *
-     * Child classes must grant that state depending on the {@link Account} is updated.
-     */
-    @Deprecated
-    protected void onAccountSet() {
-        if (getAccount() != null) {
-            storageManager = new FileDataStorageManager(getAccount(), getContentResolver());
-            capabilities = storageManager.getCapability(currentAccount.name);
-        } else {
-            Log_OC.e(TAG, "onAccountChanged was called with NULL account associated!");
-        }
-    }
-
-    @Deprecated
-    protected void setAccount(Account account) {
-        currentAccount = account;
-    }
-
-    /**
      * Getter for the capabilities of the server where the current OC account lives.
      *
      * @return Capabilities of the server where the current OC account lives. Null if the account is not
@@ -228,16 +215,7 @@ public abstract class BaseActivity extends AppCompatActivity implements Injectab
     public Account getAccount() {
         return currentAccount;
     }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        if(currentAccount != null) {
-            onAccountSet();
-        }
-    }
-
+    
     public FileDataStorageManager getStorageManager() {
         return storageManager;
     }