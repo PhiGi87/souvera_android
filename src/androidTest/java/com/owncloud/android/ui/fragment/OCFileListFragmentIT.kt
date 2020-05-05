eqilibruim-solutions/android

Remove Evernote job scheduler dependency
Signed-off-by: Chris Narkiewicz <hello@ezaquarii.com>

src/androidTest/java/com/owncloud/android/ui/fragment/OCFileListFragmentIT.kt
@@ -5,6 +5,7 @@
  * @author Tobias Kaminsky
  * Copyright (C) 2020 Tobias Kaminsky
  * Copyright (C) 2020 Nextcloud GmbH
+ * Copyright (C) 2020 Chris Narkiewicz <hello@ezaquarii.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -26,10 +27,11 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.GrantPermissionRule
-import com.evernote.android.job.JobRequest
 import com.facebook.testing.screenshot.Screenshot
 import com.nextcloud.client.account.UserAccountManagerImpl
+import com.nextcloud.client.device.BatteryStatus
 import com.nextcloud.client.device.PowerManagementService
+import com.nextcloud.client.network.Connectivity
 import com.nextcloud.client.network.ConnectivityService
 import com.nextcloud.client.preferences.AppPreferences
 import com.nextcloud.client.preferences.AppPreferencesImpl
@@ -67,16 +69,12 @@ class OCFileListFragmentIT : AbstractIT() {
     val permissionRule = GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
     private val connectivityServiceMock: ConnectivityService = object : ConnectivityService {
-        override fun isInternetWalled(): Boolean {
+        override fun isServerAvailable(): Boolean {
             return false
         }
 
-        override fun isOnlineWithWifi(): Boolean {
-            return true
-        }
-
-        override fun getActiveNetworkType(): JobRequest.NetworkType {
-            return JobRequest.NetworkType.ANY
+        override fun getConnectivity(): Connectivity {
+            return Connectivity.DISCONNECTED
         }
     }
 
@@ -87,8 +85,8 @@ class OCFileListFragmentIT : AbstractIT() {
         override val isPowerSavingExclusionAvailable: Boolean
             get() = false
 
-        override val isBatteryCharging: Boolean
-            get() = false
+        override val battery: BatteryStatus
+            get() = BatteryStatus()
     }
 
     @Test

src/main/java/com/nextcloud/client/network/ConnectivityServiceImpl.java
@@ -23,7 +23,6 @@ package com.nextcloud.client.network;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
-import com.evernote.android.job.JobRequest;
 import com.nextcloud.client.account.Server;
 import com.nextcloud.client.account.UserAccountManager;
 import com.nextcloud.client.logger.Logger;
@@ -43,7 +42,7 @@ class ConnectivityServiceImpl implements ConnectivityService {
 
     private final static String TAG = ConnectivityServiceImpl.class.getName();
 
-    private final ConnectivityManager connectivityManager;
+    private final ConnectivityManager platformConnectivityManager;
     private final UserAccountManager accountManager;
     private final ClientFactory clientFactory;
     private final GetRequestBuilder requestBuilder;
@@ -56,12 +55,12 @@ class ConnectivityServiceImpl implements ConnectivityService {
         }
     }
 
-    ConnectivityServiceImpl(ConnectivityManager connectivityManager,
+    ConnectivityServiceImpl(ConnectivityManager platformConnectivityManager,
                             UserAccountManager accountManager,
                             ClientFactory clientFactory,
                             GetRequestBuilder requestBuilder,
                             Logger logger) {
-        this.connectivityManager = connectivityManager;
+        this.platformConnectivityManager = platformConnectivityManager;
         this.accountManager = accountManager;
         this.clientFactory = clientFactory;
         this.requestBuilder = requestBuilder;
@@ -69,8 +68,9 @@ class ConnectivityServiceImpl implements ConnectivityService {
     }
 
     @Override
-    public boolean isInternetWalled() {
-        if (isOnlineWithWifi()) {
+    public boolean isServerAvailable() {
+        Connectivity c = getConnectivity();
+        if (c.isConnected() && c.isWifi()) {
 
             GetMethod get = null;
             try {
@@ -116,65 +116,37 @@ class ConnectivityServiceImpl implements ConnectivityService {
                 }
             }
         } else {
-            return getActiveNetworkType() == JobRequest.NetworkType.ANY;
+            return !getConnectivity().isConnected();
         }
 
         return true;
     }
 
     @Override
-    public boolean isOnlineWithWifi() {
+    public Connectivity getConnectivity() {
+        NetworkInfo ni;
         try {
-            NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+            ni = platformConnectivityManager.getActiveNetworkInfo();
+        } catch (Throwable t) {
+            ni = null; // no network available or no information (permission denied?)
+        }
 
-            if (activeNetwork.isConnectedOrConnecting()) {
-                switch (activeNetwork.getType()) {
-                    case ConnectivityManager.TYPE_VPN:
-                        // check if any other network is wifi
-                        for (NetworkInfo networkInfo : connectivityManager.getAllNetworkInfo()) {
-                            if (networkInfo.isConnectedOrConnecting() &&
-                                networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
-                                return true;
-                            }
-                        }
-                        return false;
-
-                    case ConnectivityManager.TYPE_WIFI:
-                        return true;
-
-                    default:
-                        return false;
-                }
-            } else {
-                return false;
-            }
-        } catch (NullPointerException exception) {
-            return false;
+        if (ni != null) {
+            boolean isConnected = ni.isConnectedOrConnecting();
+            boolean isMetered = ConnectivityManagerCompat.isActiveNetworkMetered(platformConnectivityManager);
+            boolean isWifi = ni.getType() == ConnectivityManager.TYPE_WIFI || isAnyOtherNetworkWifi();
+            return new Connectivity(isConnected, isMetered, isWifi, null);
+        } else {
+            return Connectivity.DISCONNECTED;
         }
     }
 
-    @Override
-    public JobRequest.NetworkType getActiveNetworkType() {
-        NetworkInfo networkInfo;
-        try {
-            networkInfo = connectivityManager.getActiveNetworkInfo();
-        } catch (Throwable t) {
-            return JobRequest.NetworkType.ANY;
+    private boolean isAnyOtherNetworkWifi() {
+        for (NetworkInfo ni : platformConnectivityManager.getAllNetworkInfo()) {
+            if (ni.isConnectedOrConnecting() && ni.getType() == ConnectivityManager.TYPE_WIFI) {
+                return true;
+            }
         }
-
-        if (networkInfo == null || !networkInfo.isConnectedOrConnecting()) {
-            return JobRequest.NetworkType.ANY;
-        }
-
-        boolean metered = ConnectivityManagerCompat.isActiveNetworkMetered(connectivityManager);
-        if (!metered) {
-            return JobRequest.NetworkType.UNMETERED;
-        }
-
-        if (networkInfo.isRoaming()) {
-            return JobRequest.NetworkType.CONNECTED;
-        } else {
-            return JobRequest.NetworkType.NOT_ROAMING;
-        }
+        return false;
     }
 }

src/main/java/com/owncloud/android/ui/helpers/FileOperationsHelper.java
@@ -9,7 +9,7 @@
  *
  * Copyright (C) 2015 ownCloud Inc.
  * Copyright (C) 2018 Andy Scherzinger
- * Copyright (C) 2019 Chris Narkiewicz <hello@ezaquarii.com>
+ * Copyright (C) 2020 Chris Narkiewicz <hello@ezaquarii.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -45,7 +45,6 @@ import android.util.Log;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 
-import com.evernote.android.job.JobRequest;
 import com.nextcloud.client.account.CurrentAccountProvider;
 import com.nextcloud.client.account.User;
 import com.nextcloud.client.network.ConnectivityService;
@@ -219,8 +218,7 @@ public class FileOperationsHelper {
             }
 
             // if offline or walled garden, show old version with warning
-            if (connectivityService.getActiveNetworkType() == JobRequest.NetworkType.ANY ||
-                    connectivityService.isInternetWalled()) {
+            if (!connectivityService.getConnectivity().isConnected() || connectivityService.isServerAvailable()) {
                 DisplayUtils.showSnackMessage(fileActivity, R.string.file_not_synced);
                 EventBus.getDefault().post(new SyncEventFinished(intent));
 

src/main/java/com/owncloud/android/utils/FilesSyncHelper.java
@@ -6,7 +6,7 @@
  *
  * Copyright (C) 2017 Mario Danic
  * Copyright (C) 2017 Nextcloud
- * Copyright (C) 2019 Chris Narkiewicz <hello@ezaquarii.com>
+ * Copyright (C) 2020 Chris Narkiewicz <hello@ezaquarii.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
@@ -31,7 +31,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
 
-import com.evernote.android.job.JobRequest;
 import com.nextcloud.client.account.UserAccountManager;
 import com.nextcloud.client.core.Clock;
 import com.nextcloud.client.device.PowerManagementService;
@@ -205,8 +204,7 @@ public final class FilesSyncHelper {
         }
 
         new Thread(() -> {
-            if (connectivityService.getActiveNetworkType() != JobRequest.NetworkType.ANY &&
-                    !connectivityService.isInternetWalled()) {
+            if (connectivityService.getConnectivity().isConnected() && !connectivityService.isServerAvailable()) {
                 FileUploader.retryFailedUploads(
                     context,
                     null,

PhiGi87/souvera_android

src/androidTest/java/com/owncloud/android/ui/fragment/OCFileListFragmentIT.kt
@@ -5,6 +5,7 @@
  * @author Tobias Kaminsky
  * Copyright (C) 2020 Tobias Kaminsky
  * Copyright (C) 2020 Nextcloud GmbH
+ * Copyright (C) 2020 Chris Narkiewicz <hello@ezaquarii.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -26,10 +27,11 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.GrantPermissionRule
-import com.evernote.android.job.JobRequest
 import com.facebook.testing.screenshot.Screenshot
 import com.nextcloud.client.account.UserAccountManagerImpl
+import com.nextcloud.client.device.BatteryStatus
 import com.nextcloud.client.device.PowerManagementService
+import com.nextcloud.client.network.Connectivity
 import com.nextcloud.client.network.ConnectivityService
 import com.nextcloud.client.preferences.AppPreferences
 import com.nextcloud.client.preferences.AppPreferencesImpl
@@ -71,12 +73,8 @@ class OCFileListFragmentIT : AbstractIT() {
             return false
         }
 
-        override fun isOnlineWithWifi(): Boolean {
-            return true
-        }
-
-        override fun getActiveNetworkType(): JobRequest.NetworkType {
-            return JobRequest.NetworkType.ANY
+        override fun getConnectivity(): Connectivity {
+            return Connectivity.CONNECTED_WIFI
         }
     }
 
@@ -87,8 +85,8 @@ class OCFileListFragmentIT : AbstractIT() {
         override val isPowerSavingExclusionAvailable: Boolean
             get() = false
 
-        override val isBatteryCharging: Boolean
-            get() = false
+        override val battery: BatteryStatus
+            get() = BatteryStatus()
     }
 
     @Test

src/main/java/com/nextcloud/client/network/ConnectivityServiceImpl.java
@@ -23,7 +23,6 @@ package com.nextcloud.client.network;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
-import com.evernote.android.job.JobRequest;
 import com.nextcloud.client.account.Server;
 import com.nextcloud.client.account.UserAccountManager;
 import com.nextcloud.client.logger.Logger;
@@ -43,7 +42,7 @@ class ConnectivityServiceImpl implements ConnectivityService {
 
     private final static String TAG = ConnectivityServiceImpl.class.getName();
 
-    private final ConnectivityManager connectivityManager;
+    private final ConnectivityManager platformConnectivityManager;
     private final UserAccountManager accountManager;
     private final ClientFactory clientFactory;
     private final GetRequestBuilder requestBuilder;
@@ -56,12 +55,12 @@ class ConnectivityServiceImpl implements ConnectivityService {
         }
     }
 
-    ConnectivityServiceImpl(ConnectivityManager connectivityManager,
+    ConnectivityServiceImpl(ConnectivityManager platformConnectivityManager,
                             UserAccountManager accountManager,
                             ClientFactory clientFactory,
                             GetRequestBuilder requestBuilder,
                             Logger logger) {
-        this.connectivityManager = connectivityManager;
+        this.platformConnectivityManager = platformConnectivityManager;
         this.accountManager = accountManager;
         this.clientFactory = clientFactory;
         this.requestBuilder = requestBuilder;
@@ -70,7 +69,8 @@ class ConnectivityServiceImpl implements ConnectivityService {
 
     @Override
     public boolean isInternetWalled() {
-        if (isOnlineWithWifi()) {
+        Connectivity c = getConnectivity();
+        if (c.isConnected() && c.isWifi()) {
 
             GetMethod get = null;
             try {
@@ -116,65 +116,37 @@ class ConnectivityServiceImpl implements ConnectivityService {
                 }
             }
         } else {
-            return getActiveNetworkType() == JobRequest.NetworkType.ANY;
+            return !getConnectivity().isConnected();
         }
 
         return true;
     }
 
     @Override
-    public boolean isOnlineWithWifi() {
+    public Connectivity getConnectivity() {
+        NetworkInfo networkInfo;
         try {
-            NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+            networkInfo = platformConnectivityManager.getActiveNetworkInfo();
+        } catch (Throwable t) {
+            networkInfo = null; // no network available or no information (permission denied?)
+        }
 
-            if (activeNetwork.isConnectedOrConnecting()) {
-                switch (activeNetwork.getType()) {
-                    case ConnectivityManager.TYPE_VPN:
-                        // check if any other network is wifi
-                        for (NetworkInfo networkInfo : connectivityManager.getAllNetworkInfo()) {
-                            if (networkInfo.isConnectedOrConnecting() &&
-                                networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
-                                return true;
-                            }
-                        }
-                        return false;
-
-                    case ConnectivityManager.TYPE_WIFI:
-                        return true;
-
-                    default:
-                        return false;
-                }
-            } else {
-                return false;
-            }
-        } catch (NullPointerException exception) {
-            return false;
+        if (networkInfo != null) {
+            boolean isConnected = networkInfo.isConnectedOrConnecting();
+            boolean isMetered = ConnectivityManagerCompat.isActiveNetworkMetered(platformConnectivityManager);
+            boolean isWifi = networkInfo.getType() == ConnectivityManager.TYPE_WIFI || isAnyOtherNetworkWifi();
+            return new Connectivity(isConnected, isMetered, isWifi, null);
+        } else {
+            return Connectivity.DISCONNECTED;
         }
     }
 
-    @Override
-    public JobRequest.NetworkType getActiveNetworkType() {
-        NetworkInfo networkInfo;
-        try {
-            networkInfo = connectivityManager.getActiveNetworkInfo();
-        } catch (Throwable t) {
-            return JobRequest.NetworkType.ANY;
+    private boolean isAnyOtherNetworkWifi() {
+        for (NetworkInfo networkInfo : platformConnectivityManager.getAllNetworkInfo()) {
+            if (networkInfo.isConnectedOrConnecting() && networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                return true;
+            }
         }
-
-        if (networkInfo == null || !networkInfo.isConnectedOrConnecting()) {
-            return JobRequest.NetworkType.ANY;
-        }
-
-        boolean metered = ConnectivityManagerCompat.isActiveNetworkMetered(connectivityManager);
-        if (!metered) {
-            return JobRequest.NetworkType.UNMETERED;
-        }
-
-        if (networkInfo.isRoaming()) {
-            return JobRequest.NetworkType.CONNECTED;
-        } else {
-            return JobRequest.NetworkType.NOT_ROAMING;
-        }
+        return false;
     }
 }

src/main/java/com/owncloud/android/ui/helpers/FileOperationsHelper.java
@@ -9,7 +9,7 @@
  *
  * Copyright (C) 2015 ownCloud Inc.
  * Copyright (C) 2018 Andy Scherzinger
- * Copyright (C) 2019 Chris Narkiewicz <hello@ezaquarii.com>
+ * Copyright (C) 2020 Chris Narkiewicz <hello@ezaquarii.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -45,7 +45,6 @@ import android.util.Log;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 
-import com.evernote.android.job.JobRequest;
 import com.nextcloud.client.account.CurrentAccountProvider;
 import com.nextcloud.client.account.User;
 import com.nextcloud.client.network.ConnectivityService;
@@ -219,8 +218,7 @@ public class FileOperationsHelper {
             }
 
             // if offline or walled garden, show old version with warning
-            if (connectivityService.getActiveNetworkType() == JobRequest.NetworkType.ANY ||
-                    connectivityService.isInternetWalled()) {
+            if (!connectivityService.getConnectivity().isConnected() || connectivityService.isInternetWalled()) {
                 DisplayUtils.showSnackMessage(fileActivity, R.string.file_not_synced);
                 EventBus.getDefault().post(new SyncEventFinished(intent));
 

src/main/java/com/owncloud/android/utils/FilesSyncHelper.java
@@ -6,7 +6,7 @@
  *
  * Copyright (C) 2017 Mario Danic
  * Copyright (C) 2017 Nextcloud
- * Copyright (C) 2019 Chris Narkiewicz <hello@ezaquarii.com>
+ * Copyright (C) 2020 Chris Narkiewicz <hello@ezaquarii.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
@@ -31,7 +31,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
 
-import com.evernote.android.job.JobRequest;
 import com.nextcloud.client.account.UserAccountManager;
 import com.nextcloud.client.core.Clock;
 import com.nextcloud.client.device.PowerManagementService;
@@ -205,8 +204,7 @@ public final class FilesSyncHelper {
         }
 
         new Thread(() -> {
-            if (connectivityService.getActiveNetworkType() != JobRequest.NetworkType.ANY &&
-                    !connectivityService.isInternetWalled()) {
+            if (connectivityService.getConnectivity().isConnected() && !connectivityService.isInternetWalled()) {
                 FileUploader.retryFailedUploads(
                     context,
                     null,