nextcloud/android

test(doc-scan) Add test for reflection-based appscan check
Testing two main variants, one without appscan, and one with it. However,
as of now the automated test for the gplay flavor does not run
automatically in the CI, as we only test the generic flavor. So the test's
purpose was just to verify the reflection approach locally.

Signed-off-by: Philipp Hasper <vcs@hasper.info>

app/src/androidTestGeneric/java/com/nextcloud/client/di/VariantModuleTest.kt
@@ -1,0 +1,57 @@
+/*
+ * Nextcloud - Android Client
+ *
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package com.nextcloud.client.di
+
+import com.nextcloud.client.documentscan.AppScanOptionalFeature
+import dagger.Component
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Unit test for [VariantModule] that tests the reflection-based approach
+ * to conditionally load the ScanPageContract.
+ */
+class VariantModuleTest {
+
+    private lateinit var component: TestVariantComponent
+
+    @Before
+    fun setup() {
+        component = DaggerVariantModuleTest_TestVariantComponent.create()
+    }
+
+    /**
+     * In this variant, app scan should not be available
+     */
+    @Test
+    fun testAppScanOptionalFeatureAvailability() {
+        val feature = component.appScanOptionalFeature()
+
+        assertFalse(feature.isAvailable)
+        assertEquals(AppScanOptionalFeature.Stub, feature)
+
+        // Verify that calling getScanContract on stub throws UnsupportedOperationException
+        try {
+            feature.getScanContract()
+            throw AssertionError("Expected UnsupportedOperationException")
+        } catch (e: UnsupportedOperationException) {
+            assertTrue(e.message?.contains("not available") == true)
+        }
+    }
+
+    /**
+     * Dagger component for testing VariantModule in isolation
+     */
+    @Component(modules = [VariantModule::class])
+    interface TestVariantComponent {
+        fun appScanOptionalFeature(): AppScanOptionalFeature
+    }
+}

app/src/androidTestGplay/java/com/nextcloud/client/di/VariantModuleTest.kt
@@ -1,0 +1,54 @@
+/*
+ * Nextcloud - Android Client
+ *
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package com.nextcloud.client.di
+
+import com.nextcloud.client.documentscan.AppScanOptionalFeature
+import dagger.Component
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Unit test for [VariantModule] that tests the reflection-based approach
+ * to conditionally load the ScanPageContract.
+ */
+class VariantModuleTest {
+
+    private lateinit var component: TestVariantComponent
+
+    @Before
+    fun setup() {
+        component = DaggerVariantModuleTest_TestVariantComponent.create()
+    }
+
+    /**
+     * In this variant, app scan should be available
+     */
+    @Test
+    fun testAppScanOptionalFeatureAvailability() {
+        val feature = component.appScanOptionalFeature()
+
+        assertTrue(feature.isAvailable)
+        assertNotEquals(AppScanOptionalFeature.Stub, feature)
+
+        // Verify that calling getScanContract returns without raising an exception
+        assertNotNull(feature.getScanContract())
+    }
+
+    /**
+     * Dagger component for testing VariantModule in isolation
+     */
+    @Component(modules = [VariantModule::class])
+    interface TestVariantComponent {
+        fun appScanOptionalFeature(): AppScanOptionalFeature
+    }
+}

PhiGi87/souvera_android

app/src/androidTestGeneric/java/com/nextcloud/client/di/VariantModuleTest.kt
@@ -1,0 +1,50 @@
+/*
+ * Nextcloud - Android Client
+ *
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package com.nextcloud.client.di
+
+import com.nextcloud.client.documentscan.AppScanOptionalFeature
+import dagger.Component
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Unit test for [VariantModule] that tests the reflection-based approach
+ * to conditionally load the ScanPageContract.
+ */
+class VariantModuleTest {
+
+    private lateinit var component: TestVariantComponent
+
+    @Before
+    fun setup() {
+        component = DaggerVariantModuleTest_TestVariantComponent.create()
+    }
+
+    @Test
+    fun testAppScanWhenNotAvailableShouldReturnError() {
+        val feature = component.appScanOptionalFeature()
+
+        assertFalse(feature.isAvailable)
+        assertEquals(AppScanOptionalFeature.Stub, feature)
+
+        try {
+            feature.getScanContract()
+            throw AssertionError("Expected UnsupportedOperationException")
+        } catch (e: UnsupportedOperationException) {
+            assertTrue(e.message?.contains("not available") == true)
+        }
+    }
+
+    @Component(modules = [VariantModule::class])
+    interface TestVariantComponent {
+        fun appScanOptionalFeature(): AppScanOptionalFeature
+    }
+}

app/src/androidTestGplay/java/com/nextcloud/client/di/VariantModuleTest.kt
@@ -1,0 +1,47 @@
+/*
+ * Nextcloud - Android Client
+ *
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package com.nextcloud.client.di
+
+import com.nextcloud.client.documentscan.AppScanOptionalFeature
+import dagger.Component
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Unit test for [VariantModule] that tests the reflection-based approach
+ * to conditionally load the ScanPageContract.
+ */
+class VariantModuleTest {
+
+    private lateinit var component: TestVariantComponent
+
+    @Before
+    fun setup() {
+        component = DaggerVariantModuleTest_TestVariantComponent.create()
+    }
+
+    @Test
+    fun testAppScanWhenAvailableShouldReturnContract() {
+        val feature = component.appScanOptionalFeature()
+
+        assertTrue(feature.isAvailable)
+        assertNotEquals(AppScanOptionalFeature.Stub, feature)
+
+        assertNotNull(feature.getScanContract())
+    }
+
+    @Component(modules = [VariantModule::class])
+    interface TestVariantComponent {
+        fun appScanOptionalFeature(): AppScanOptionalFeature
+    }
+}